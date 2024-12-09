60d3a97e4e0eaa8e09bd0db541fe08d38e0aa6db
florian <florian.lesp@gmail.com>
2024-12-09 22:21:01 +0100

    feat: add and delete participants

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue'
+import type { Ref } from 'vue'
+import { TrashIcon } from '@heroicons/vue/20/solid'
 
+const nbParticipant = ref(0)
+const participants: Ref<Array<string>> = ref([])
+
+function deleteParticipant(index: number) {
+  participants.value.splice(index, 1)
+  nbParticipant.value = nbParticipant.value - 1
+}
+
+watch(nbParticipant, () => {
+  participants.value = []
+  for (let index = 0; index < nbParticipant.value; index++) {
+    let result = index + 1
+    participants.value.push(result.toString())
+  }
+})
 </script>
 
 <template>
@@ -8,12 +26,32 @@
       <h1 class="p-10 text-5xl">Qui offre quoi ?</h1>
 
       <div class="flex items-center space-x-4">
-        <label for="">Sélectionnez le nombre de participant:</label>
+        <label for="">Sélectionnez le nombre de participants:</label>
         <input
           type="number"
           class="w-20 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-          value="0"
+          v-model="nbParticipant"
         />
+      </div>
+
+      <div v-if="participants.length > 0" class="flex flex-col">
+        <div
+          v-for="(participant, index) in participants"
+          :key="participant"
+          class="flex items-center space-x-4 mt-5"
+        >
+          <input
+            type="text"
+            class="w-64 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+            :placeholder="`Participant n°${participant}`"
+          />
+          <button
+            @click="deleteParticipant(index)"
+            class="flex items-center justify-center rounded-md bg-red-600 p-2 text-white hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-600"
+          >
+            <TrashIcon class="h-5 w-5" />
+          </button>
+        </div>
       </div>
     </div>
   </div>
