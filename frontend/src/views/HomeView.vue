2fb795e867a749669003ff673014899c9a503e81
florian <florian.lesp@gmail.com>
2024-12-12 23:05:41 +0100

    feat: add vmodel to inputs to insert participants

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -46,6 +46,7 @@ watch(nbParticipant, (newValue, oldValue) => {
             type="text"
             class="w-64 bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-slate-200 rounded-md px-3 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
             :placeholder="participant"
+            v-model="participants[index]"
           />
 
           <button
